8a4b5c0c0bc35faa7941ccd4537ed7e72a1e6218
jinwoo <yasisicoco@gmail.com>
2024-05-16 17:39:34 +0900

    initial commit

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -4,18 +4,12 @@
     <form @submit.prevent="signUp">
       <label for="username">username: </label>
       <input type="text" id="username" v-model.trim="username"><br>
-      
-      <label for="email">email: </label>
-      <input type="text" id="email" v-model.trim="email"><br>
 
       <label for="password1">password: </label>
       <input type="text" id="password1" v-model.trim="password1"><br>
       
       <label for="password2">password confirmation: </label>
       <input type="text" id="password2" v-model.trim="password2"><br>
-
-      <label for="nickname">nickname: </label>
-      <input type="text" id="nickname" v-model.trim="nickname"><br>
 
       <input type="submit" value="SignUp">
     </form>
@@ -26,19 +20,15 @@
 import { ref } from 'vue'
 import { useUserStore } from '@/stores/userStore'
 const store = useUserStore()
-const username = ref(null)
-const password1 = ref(null)
-const password2 = ref(null)
-const email = ref(null)
-const nickname = ref(null)
+const username = ref('')
+const password1 = ref('')
+const password2 = ref('')
 
 const signUp = function () {
   const payload = {
     username: username.value,
     password1: password1.value,
     password2: password2.value,
-    email: email.value,
-    nickname: nickname.value
   }
   store.signUp(payload)
 }
