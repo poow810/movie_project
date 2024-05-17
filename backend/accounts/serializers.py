9cea59dbe4edb49ca43ce931f4fb4a418cf590c2
hawoon <poow810@naver.com>
2024-05-17 14:20:41 +0900

    feat: Update accounts
    # 본문은 아래에 작성
    회원 마무리
    # 꼬릿말은 아래에 작성: ex) Github issue #23
    Resolves : #1
    # --- COMMIT END ---
    #   <타입> 리스트
    #   feat        : 기능 (새로운 기능)
    #   fix         : 버그 (버그 수정)
    #   refactor    : 리팩토링
    #   design      : CSS 등 사용자 UI 디자인 변경
    #   comment     : 필요한 주석 추가 및 변경
    #   style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs        : 문서 수정 (문서 추가, 수정, 삭제, README)
    #   test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등)
    #   init        : 초기 생성
    #   rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    #   remove      : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   제목 첫 글자를 대문자로
    #   제목은 명령문으로
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기
    #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------
    #   <꼬리말>
    #   필수가 아닌 optioanl
    #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우)
    #   Resolves     : 이슈 해결했을 때 사용
    #   Ref          : 참고할 이슈가 있을 때 사용
    #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    #   ex) Fixes: #47 Related to: #32, #21

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.serializers import UserDetailsSerializer
+
+
+User = get_user_model()
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    nickname = serializers.CharField(
+      required=False,
+      allow_blank=True,
+      max_length=255
+    )
+
+    def get_cleaned_data(self):
+        return {
+            'username': self.validated_data.get('username', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'nickname': self.validated_data.get('nickname', ''),
+            'email': self.validated_data.get('email', ''),
+        }
+    
+
+class CustomUserDetailsSerializer(UserDetailsSerializer):
+    class Meta:
+        extra_fields = []
+        # see https://github.com/iMerica/dj-rest-auth/issues/181
+        # UserModel.XYZ causing attribute error while importing other
+        # classes from `serializers.py`. So, we need to check whether the auth model has
+        # the attribute or not
+        if hasattr(User, 'USERNAME_FIELD'):
+            extra_fields.append(User.USERNAME_FIELD)
+        if hasattr(User, 'EMAIL_FIELD'):
+            extra_fields.append(User.EMAIL_FIELD)
+        if hasattr(User, 'first_name'):
+            extra_fields.append('first_name')
+        if hasattr(User, 'last_name'):
+            extra_fields.append('last_name')
+        if hasattr(User, 'nickname'):
+            extra_fields.append('nickname')    
+        model = User
+        fields = ('pk', *extra_fields)
+        read_only_fields = ('email',)
