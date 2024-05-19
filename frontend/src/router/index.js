f92548c1df9da175a211903189c3249e32feafb9
jinwoo <yasisicoco@gmail.com>
2024-05-19 15:38:23 +0900

    comment: route 주석추가
    # 본문은 아래에 작성
    회원 가입 및 로그인 기능 추가.
    게시글 생성 기능 추가
    # 꼬릿말은 아래에 작성: ex) Github issue #23
    Resolves : #1
    # --- COMMIT END ---
    #   <타입> 리스트
    #   feat        : 기능 (새로운 기능)
    #   fix         : 버그 (버그 수정)
    #   refactor    : 리팩토링
    #   design      : CSS 등 사용자 UI 디자인 변경
    #   comment     : 필요한 주석 추가 및 변경
    #   style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs        : 문서 수정 (문서 추가, 수정, 삭제, README)
    #   test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등)
    #   init        : 초기 생성
    #   rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    #   remove      : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   제목 첫 글자를 대문자로
    #   제목은 명령문으로
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기
    #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------
    #   <꼬리말>
    #   필수가 아닌 optioanl
    #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우)
    #   Resolves     : 이슈 해결했을 때 사용
    #   Ref          : 참고할 이슈가 있을 때 사용
    #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    #   ex) Fixes: #47 Related to: #32, #21

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,22 +14,20 @@ import CommuArticleDetail from '@/views/community/CommuArticleDetail.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // ACCOUNT
     {
       path: '/',
       name: 'home',
       component: HomeView
     },
+
+    // ACTOR
     {
-      path: '/signup',
-      name: 'signup',
-      component: SignUpView
+      path: '/actors',
+      name: 'actor',
+      component: ActorView
     },
-    {
-      path: '/login',
-      name: 'login',
-      component: LogInView
-    },
+
+    // COMMUNITY
     {
       path: '/community',
       name: 'community',
@@ -41,20 +39,12 @@ const router = createRouter({
       component: CommuArticleCreateView
     },
     {
-      path: '/accounts/logout',
-      name: 'logout',
-      component: LogOug
+      path: '/community/:id',
+      name: 'articleDetail',
+      component: CommuArticleDetail
     },
-    {
-      path: '/accounts/passwordchange',
-      name: 'passwordchange',
-      component: PasswordChange
-    },
-    {
-      path: '/actors',
-      name: 'actor',
-      component: ActorView
-    },
+
+    // RECOMMEND
     {
       path: '/movies/genreselect',
       name: 'genreselect',
@@ -65,11 +55,29 @@ const router = createRouter({
       name: 'recommend',
       component: MovieRecomView
     },
+
+
+    // ACCOUNT
     {
-      path: '/community/:id',
-      name: 'articleDetail',
-      component: CommuArticleDetail
-    }
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LogInView
+    },
+    {
+      path: '/accounts/logout',
+      name: 'logout',
+      component: LogOug
+    },
+    {
+      path: '/accounts/passwordchange',
+      name: 'passwordchange',
+      component: PasswordChange
+    },
   ]
 })
 
