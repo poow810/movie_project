bfcceaf72e585833cc79aa71a391b8f858e5c164
hawoon <poow810@naver.com>
2024-05-20 17:55:21 +0900

    merge
    머지
    # 꼬릿말은 아래에 작성: ex) Github issue #23
    Resolves : #1
    # --- COMMIT END ---
    #   <타입> 리스트
    #   feat        : 기능 (새로운 기능)
    #   fix         : 버그 (버그 수정)
    #   refactor    : 리팩토링
    #   design      : CSS 등 사용자 UI 디자인 변경
    #   comment     : 필요한 주석 추가 및 변경
    #   style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs        : 문서 수정 (문서 추가, 수정, 삭제, README)
    #   test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등)
    #   init        : 초기 생성
    #   rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    #   remove      : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    #   제목 첫 글자를 대문자로
    #   제목은 명령문으로
    #   제목 끝에 마침표(.) 금지
    #   제목과 본문을 한 줄 띄워 분리하기
    #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------
    #   <꼬리말>
    #   필수가 아닌 optioanl
    #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우)
    #   Resolves     : 이슈 해결했을 때 사용
    #   Ref          : 참고할 이슈가 있을 때 사용
    #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우)
    #   ex) Fixes: #47 Related to: #32, #21
     main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
 import { computed } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 import { useUserStore } from '@/stores/userStore'
+
 const userStore = useUserStore()
 
 const id = computed(() => {
@@ -15,34 +16,48 @@ const logOut = function () {
 const isLogIn = computed(() => {
   return userStore.isLogIn
 })
-
-
 </script>
 
 <template>
   <header>
-    <div>
-      <nav v-if="isLogIn">
-        <RouterLink to="/">Home</RouterLink> |
-        <RouterLink :to="{ name: 'actor' }">배우</RouterLink> |
-        <RouterLink :to="{ name: 'community' }">커뮤니티</RouterLink> |
-        <!-- recomStore.userSetGenre가 존재하면 'genreselect'로, 그렇지 않으면 'recommend'로 라우팅 -->
-        <RouterLink :to="{ name: 'recommend' }">영화추천</RouterLink> |
-        <RouterLink :to="{ name: 'profile', params: {'id': id}}">프로필</RouterLink> |
-        <button @click="logOut">로그아웃</button>
-      </nav>
-      <nav v-else>
-        <RouterLink to="/">Home</RouterLink> |
-        <RouterLink :to="{ name: 'community' }">커뮤니티</RouterLink> |
-        <RouterLink :to="{ name: 'signup' }">회원가입</RouterLink> |
-        <RouterLink :to="{ name: 'login' }">로그인</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar navbar-expand-lg" style="background-color: #1b1b1b;">
+      <div class="container-fluid">
+        <RouterLink class="navbar-brand fw-bold text-white" to="/">MOVIE</RouterLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item" v-if="isLogIn">
+              <RouterLink class="nav-link text-white" :to="{ name: 'actor' }">배우</RouterLink>
+              <RouterLink class="nav-link text-white" :to="{ name: 'recommend' }">영화추천</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link text-white" :to="{ name: 'community' }">커뮤니티</RouterLink>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item" v-if="isLogIn">
+              <RouterLink class="nav-link text-white" :to="{ name: 'profile', params: {'id': id}}">프로필</RouterLink>
+              <button class="btn btn-outline-success" @click="logOut">로그아웃</button>
+            </li>
+            <li class="nav-item" v-else>
+              <RouterLink class="nav-link text-white" :to="{ name: 'signup' }">회원가입</RouterLink>
+              <RouterLink class="nav-link text-white" :to="{ name: 'login' }">로그인</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
 
+
+<style scoped>
+.navbar-nav .nav-item {
+  display: flex;
+}
 </style>
